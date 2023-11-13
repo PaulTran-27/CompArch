c575e339ac46b3e60165640d56c2960520adcf03
PaulTran-27 <paultran0910@gmail.com>
2023-11-13 16:49:20 +0700

    Implement grid setup parser

--- bitmap.asm
+++ bitmap.asm
@@ -1,18 +1,22 @@
 .data
-bitmap: .space 65536      # 512 * 512 / 4
+bitmap: .space 128     # 512 * 256 / 4
 
 .text
 main:
     # Initialize the bitmap with all pixels set to white
     la $t1, bitmap
+    li $t2, 128
+    li $t0, 0xFFFFFFFF
     fill:
-
-        li $t0, 0xFFFFFFFF     # Set all bits to 1 (white)
+    	beqz $t2, exit
+	     # Set all bits to 1 (white)
     	sw $t0, 0($t1)         # Store the pattern in the bitmap
+	subi $t0, $t0, 0xAAAA
 	addi $t1, $t1, 4
+	addi $t2, $t2, -1
 	j fill
     # Continue initializing other rows as needed
-
+    exit:
     # Display the bitmap
-    li $v0, 10             # Exit program
-    syscall
+    	li $v0, 10             # Exit program
+    	syscall
--- mips1.asm
+++ mips1.asm
@@ -1,10 +1,11 @@
 .data
-	greet: .asciiz "Hello player(s) \n"
+	dash: .asciiz "--------------"
+	greet: .asciiz "Hello player(s)"
 	turn_count_prompt: .asciiz "Turn number: "
 	turn_count: .word 0
 	p1: .asciiz "Player one's turn: "
 	p2: .asciiz "Player two's turn: "
-	input: .asciiz "Please setup your battleship strategy:"
+	input: .asciiz "Please setup your battleship strategy: \n"
 	grid_player_1: .byte 0:49 
 	grid_player_2: .byte 0:49 # create 7 by 7 grid for each player
 	seven: .word 7
@@ -12,14 +13,39 @@
 	txt: .asciiz "ENDING PROGRAM\n"
 	txt_1: .asciiz "Confirm placement ? \n"
 	input_buffer: .space 100 # buffer for input
+	exception_input: .asciiz "Invalid input: "
+	setup_except: .asciiz "Valid character for setup are 0, 1 and whitespace only \n"
+	try_again: .asciiz "Please try again: "
 .text
 	main:
-		la $a0, greet
+		# start of the game
 		li $v0, 4
+		la $a0, dash
 		syscall
-	
+		la $a0, greet
+		syscall
+		la $a0, dash
+		syscall
+		la $a0, endl
+		syscall
 		
-		la $a0, grid_player_2
+		## Input
+		la $a0, input
+		syscall
+	p1_setup:
+		la $a0, p1
+		syscall 
+
+		la $a0, input_buffer
+		li $a1, 100  
+		li $v0, 8
+		syscall 
+		
+		li $a2, 1
+		la $a3, p1_setup
+		jal parse_input
+		 
+		la $a0, grid_player_1
 		jal print_grid
 		
 
@@ -30,8 +56,70 @@
 		syscall
 		li $v0, 10
 		syscall
+	
+	load_grid:
+		#input a2; load p1 grid to a2
+		beq $a2, 1, return_1
+		la $a2, grid_player_2
+		jr $ra
+		return_1:
+			la $a2, grid_player_1
+			jr $ra
 		
 		
+	parse_input:
+		# input: a0 -> input_buffer; a1 -> size; a2 -> player id
+		#	  a3 -> address to jump to in case of exception
+		addi $sp, $sp, -8
+		sw $s0, 0($sp)
+		sw $ra, 4($sp)
+		
+		# while
+		la $s0, input_buffer
+		jal load_grid
+		li $t7, 49
+		addi $t6, $a0, 0 #limiter for no infinite loop
+		parse_while:
+			beqz $t6, return_grid
+			addi $t6, $t6, -1
+			beqz $t7, return_grid
+
+			lb   $t0, 0($s0)
+			beq $t0, 10, return_grid 
+			subi $t0, $t0, 48
+			addi $s0, $s0, 1
+			
+			beq $t0, 1, init_grid
+	        	beq $t0, 0, init_grid
+	        	
+	        	bne $t0, -16, throw_invalid_setup
+	        	
+	        	#li $v0, 11
+			#li $a0, 32
+			#syscall
+	        	j parse_while
+		
+			init_grid:
+				sb $t0, 0($a2)
+				li $v0, 1
+				add $a0, $t0, $zero
+				syscall
+				addi $a2, $a2, 1
+				addi $t7, $t7, -1
+				j parse_while
+		
+		return_grid:
+			lw $s0, 0($sp)
+			lw $ra, 4($sp)
+			addi $sp, $sp, 8
+			jr $ra		
+		throw_invalid_setup:
+			li $v0, 1
+			add $a0, $t0, $zero
+			syscall 
+			la $a0, setup_except
+			la $a1, ($a3)
+			j throw_invalid_input
 	print_grid:
 		#input a0: player grid address
 		addi $sp, $sp, -4
@@ -73,6 +161,17 @@
 			lw $s0, 0($sp)
 			addi $sp, $sp, 4
 			jr $ra
+	throw_invalid_input:
+		# input: a0 -> address of exception type,
+		#   	 a1 -> address to return after catch exception
+		la $t0, ($a0)
+		li $v0, 4
+		la $a0, exception_input
+		syscall
+		la $a0, ($t0)
+		syscall
+		la $a0, try_again
+		jr $a1
 	update_turn_counter:
 		addi $sp, $sp, -4
 		sw $s0, 0($sp)
