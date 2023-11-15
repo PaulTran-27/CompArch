b1dc0ae12347e3faf28bcd3732f27d2b31826f63
PaulTran-27 <paultran0910@gmail.com>
2023-11-15 08:41:48 +0700

    Starting implement exception handling

--- mips1.asm
+++ mips1.asm
@@ -14,7 +14,7 @@
 	txt_1: .asciiz "Confirm placement ? \n"
 	input_buffer: .space 100 # buffer for input
 	exception_input: .asciiz "Invalid input: "
-	setup_except: .asciiz "Valid character for setup are 0, 1 and whitespace only \n"
+	setup_except: .asciiz "Valid characters for setup are number in range 0 to 6 and whitespace only \n"
 	try_again: .asciiz "Please try again: "
 	remaining: .asciiz "\nRemaining ship(s): \n"
 	s_21: .asciiz "		2 by 1 ship(s): "
@@ -26,6 +26,9 @@
 	player_1_ships: .word 3, 2, 1 # 2x1, 3x1 4x1
 	player_2_ships: .word 3, 2, 1 # 2x1, 3x1 4x1
 	choose_placement: .asciiz "Input placement by format x_0, y_0, x_1, y_1: "
+	input_xy_xy: .word 0:4
+	ship_misalign: .asciiz "Ship not in horizontal or vertical alignment.\n"
+	ship_size_mismatch: .asciiz "Coordinates does not match the input ship size. \n"
 .text
 	main:
 		# start of the game
@@ -66,7 +69,7 @@
 			# print 3x1 ships
 
             la $a0, s_31
-		        syscall
+		    syscall
 
 			lw $t0, 0($s7)
 
@@ -101,17 +104,24 @@
 			syscall
 			jal is_type_valid
 			
+			addi $sp, $sp, -4
+			sw $t0, 0($sp)
+			addi $t0, $v0, 0
+
 			li $v0, 4
 			la $a0, choose_placement
 			syscall
-			# la $a0, input_buffer
-			# li $a1, 100  
-			# li $v0, 8
-			# syscall 
+			la $a0, input_buffer
+			li $a1, 100
+			li $v0, 8
+			syscall 
 			
-			# li $a2, 1
-			# la $a3, p1_setup
-			# jal parse_input
+			addi $a1, $t0, 0
+			lw $t0, 0($sp)
+			addi $sp, $sp, 4
+			li $a2, 1
+			la $a3, p1_setup
+			jal parse_input
 			
 		la $a0, grid_player_1
 		jal print_grid
@@ -124,7 +134,7 @@
 		syscall
 			
 	is_type_valid:
-        	addi $sp, $sp, -4
+        addi $sp, $sp, -4
 		sw $s0, 0($sp)
 		addi $t0, $v0, -2
 		addi $t1, $a1, 0
@@ -175,53 +185,147 @@
 		addi $sp, $sp, -8
 		sw $s0, 0($sp)
 		sw $ra, 4($sp)
-		
+		addi $s6, $a1, 0
 		# while
 		la $s0, input_buffer
 		jal load_grid
-		li $t7, 49
-		addi $t6, $a0, 0 #limiter for no infinite loop
+		li $t7, 0
+		li $t6, 100#limiter for no infinite loop
 		parse_while:
 			beqz $t6, return_grid
 			addi $t6, $t6, -1
-			beqz $t7, return_grid
+			beq $t7, 4, init_grid
 
 			lb   $t0, 0($s0)
-			beq $t0, 10, return_grid 
-			subi $t0, $t0, 48
+			beq  $t0, 10, return_grid 
+			addi $t0, $t0, -48
 			addi $s0, $s0, 1
 			
-			beq $t0, 1, init_grid
-	        	beq $t0, 0, init_grid
-	        	
-	        	bne $t0, -16, throw_invalid_setup
-	        	
-	        	#li $v0, 11
+			slt $t1, $t0, $zero
+			sne $t2, $t0, -16
+			and $t1, $t1, $t2
+			
+			sgt $t2, $t0,  6
+			# check if input is in range [0,6]
+			or  $t1, $t1, $t2
+			bnez $t1,    throw_invalid_setup
+			
+			# if not space run update coordinates
+			bne $t0, -16,  update_coordinates
+	        
+	        #li $v0, 11
 			#li $a0, 32
 			#syscall
-	        	j parse_while
-		
+	        j parse_while
+
+			update_coordinates:
+				addi $sp, $sp, -8
+				sw $s0, 0($sp)
+				sw $t7, 4($sp)
+				la $s0, input_xy_xy
+                sll $t7, $t7, 2
+				add $s0, $s0, $t7
+				sw $t0, 0($s0)
+				
+				# restore stack
+				lw $s0, 0($sp)
+				lw $t7, 4($sp)
+				addi $sp, $sp, 8
+
+				#increment number of inputed coordinate
+				addi $t7, $t7, 1
+                j parse_while
+
+
 			init_grid:
-				sb $t0, 0($a2)
-				li $v0, 1
-				add $a0, $t0, $zero
-				syscall
-				addi $a2, $a2, 1
-				addi $t7, $t7, -1
-				j parse_while
-		
+				# sb $t0, 0($a2)
+				# li $v0, 1
+				# add $a0, $t0, $zero
+				# syscall
+				# addi $a2, $a2, 1
+				# addi $t7, $t7, -1
+				# j parse_while
+
+				# 0($s0) x 4($s0) y 8... 12 ... 
+				addi $sp, $sp, -36
+				sw $s0, 0($sp)
+				sw $t0, 4($sp)
+				sw $t1, 8($sp)
+				sw $t2, 12($sp)
+				sw $t3, 16($sp)
+				sw $t4, 20($sp)
+				sw $t5, 24($sp)
+				sw $t6, 28($sp)
+				sw $t7, 32($sp)
+                la $s0, input_xy_xy
+				# check if head and tail coordinates align
+				lw $t0,  0($s0) # x1
+				lw $t1,  4($s0) # y1
+				lw $t2,  8($s0) # x2
+				lw $t3, 12($s0) # y2
+
+				seq $t4, $t0, $t2
+				seq $t5, $t1, $t3
+
+				or $t4, $t4, $t5
+				# if [(x == x) or (y == y)] not true throw exception
+				li $v0, 0
+				beqz $t4, restore_st
+
+				# if y_1 == y_2 calc length base on x_s else y_s
+				beqz $t5, do_else
+
+				sub $t7, $t2, $t0 # length
+	
+				j end_if
+			do_else: 
+				sub $t7, $t3, $t1
+			end_if:
+				slt $t6, $t7, $zero # sign-number
+				beqz $t6, positive # if (length < 0) return 1
+				mul $t7, $t7, -1
+			positive:
+				li  $v0, 1
+				bne $t7, $s6, restore_st # if length not match with the chosen boat: exception
+
+			restore_st:
+				# restore stack
+                lw $s0, 0($sp)
+				lw $t0, 4($sp)
+				lw $t1, 8($sp)
+				lw $t2, 12($sp)
+				lw $t3, 16($sp)
+				lw $t4, 20($sp)
+				lw $t5, 24($sp)
+				lw $t6, 28($sp)
+				lw $t7, 32($sp)
+				addi $sp, $sp, 36
+				beqz $v0, throw_invalid_ship
+				beq  $v0, 1, size_mismatch
 		return_grid:
 			lw $s0, 0($sp)
 			lw $ra, 4($sp)
 			addi $sp, $sp, 8
 			jr $ra		
 		throw_invalid_setup:
-			li $v0, 1
-			add $a0, $t0, $zero
-			syscall 
+			# li $v0, 1
+			# add $a0, $t0, $zero
+			# syscall 
 			la $a0, setup_except
 			la $a1, ($a3)
 			j throw_invalid_input
+		throw_invalid_ship:
+			# li $v0, 1
+			# add $a0, $t0, $zero
+			# syscall 
+			la $a0, ship_misalign
+			la $a1, ($a3)
+			j throw_invalid_input
+		size_mismatch:
+			la $a0, ship_size_mismatch
+			la $a1, ($a3)
+            j throw_invalid_input
+
 	print_grid:
 		#input a0: player grid address
 		addi $sp, $sp, -4
